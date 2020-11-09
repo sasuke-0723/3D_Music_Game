1b4db8f52a3d03147634476d3f6569777574f625
sasuke-0723 <kouki072817@gmail.com>
2020-11-09 12:22:33 +0900

    touch判定のテストプレイが終わった

---
+++ Assets/Scripts/TapManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapManager : MonoBehaviour
+{
+    private Vector3 touchStartPos;
+    private Vector3 touchEndPos;
+
+    private void Update()
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    Debug.Log("Touch: " + touch.fingerId);
+                    break;
+                case TouchPhase.Stationary:
+                    Debug.Log("Hold:" + touch.fingerId);
+                    break;
+                case TouchPhase.Moved:
+                    Debug.Log("Slide:" + touch.fingerId);
+                    break;
+                case TouchPhase.Ended:
+                    Debug.Log("HoldEnd:" + touch.fingerId);
+                    break;
+                case TouchPhase.Canceled:
+                    Debug.Log("Cancel:" + touch.fingerId);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
